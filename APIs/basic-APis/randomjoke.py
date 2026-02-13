c6473d8bc2be2dcc47aabbd862d416c5dda3379a
SJcodeML <144626657+SJcodeML@users.noreply.github.com>
2026-02-13 19:50:11 +0500

    code added

--- APIs/basic-APis/randomjoke.py
+++ APIs/basic-APis/randomjoke.py
@@ -7,19 +7,24 @@
 # json course of mumshad mannambeth 
 import requests 
 
+# def main():
+#     response = requests.get('https://api.freeapi.app/api/v1/public/randomjokes?limit=10&query=science&inc=categories%2Cid%2Ccontent&page=1')
+#     result = response.json()
+#     # print(result['data']['data'][0]['content'])
+#     print(result['data']['previousPage'])
+#     # print (str(result))
+#     # print()
+
+
+
+
 def main():
-    response = requests.get('https://api.freeapi.app/api/v1/public/randomjokes?limit=10&query=science&inc=categories%2Cid%2Ccontent&page=1')
-    result = response.json()
-    print(result['data']['data'][0]['content'])
-    # print (str(result))
-    # print()
+    response =requests.get('https://api.freeapi.app/api/v1/public/books?page=1&limit=10&inc=kind%2Cid%2Cetag%2CvolumeInfo&query=tech')
+    result= response.json()
+    print(result['data']['data'][0]['volumeInfo'])
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
 
 
