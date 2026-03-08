1ff6c71d6d841fb9c6ac315b644ff5f33db8f24f
SJcodeML <144626657+SJcodeML@users.noreply.github.com>
2026-03-08 05:15:06 +0500

    project added

--- Projects/01_Homework_Projects/03_If_statement/03_leap_year/main.py
+++ Projects/01_Homework_Projects/03_If_statement/03_leap_year/main.py
@@ -1,23 +1,23 @@
 
-def main():
-    # Get the year to check from the user
-    year = int(input('Please input a year: '))
-
-    if year % 4 == 0:  # Checking whether the provided year is evenly divisibly by 4
-        if year % 100 == 0:  # Checking whether the provided year is evenly divisibly by 100
-            if year % 400 == 0:  # Checking whether the provided year is evenly divisibly by 400
-                print("That's a leap year!")
-            else:  # (Not divisible by 400)
-                print("That's not a leap year.")
-        else:  # (Not divisible by 100)
-            print("That's a leap year!")
-    else:  # (Not divisible by 4)
-        print("That's not a leap year.")
 
 
-# There is no need to edit code beyond this point
+
+
+
+def main ():
+    try:
+        user_year = int(input("Please enter year?  ")) 
+    except ValueError:
+        print("Invalid year")
+        return
+
+    if (user_year % 4 == 0  and user_year % 100 != 0 ) or (user_year % 400 == 0 ):
+        print("That's  a leap year")
+    else :
+        print("That's not a leap year")    
+
 
 
 
 if __name__ == "__main__":
-    main()
+    main()        
