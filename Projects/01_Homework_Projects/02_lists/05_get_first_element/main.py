1c87448767cfcafe3ac1cc56972bfbb3049fa307
SJcodeML <144626657+SJcodeML@users.noreply.github.com>
2026-03-08 22:32:16 +0500

    project added

---
+++ Projects/01_Homework_Projects/02_lists/05_get_first_element/main.py
@@ -0,0 +1,27 @@
+## Problem Statement
+'''
+Fill out the function get_first_element(lst) which takes in a list lst as a parameter and prints 
+the first element in the list. The list is guaranteed to be non-empty. We've written some code for
+you which prompts the user to input the list one element at a time.
+'''
+
+
+def first_element (numbers):
+    
+        first_element = numbers[0]
+        return first_element
+
+
+
+def main():
+    non_empty_list : [1, 2, 4, 5 ,"sidra"]
+    if non_empty_list != "":
+        result = first_element(non_empty_list)
+        print(result)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
