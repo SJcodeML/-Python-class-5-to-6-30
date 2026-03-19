e8b1f2eaa8521b7e24c63f7d802dad3ea799a07a
SJcodeML <144626657+SJcodeML@users.noreply.github.com>
2026-03-19 19:06:59 +0500

    project added

---
+++ Projects/01_Homework_Projects/02_lists/06_get_last_element/README.md
@@ -0,0 +1,22 @@
+## Problem Statement
+
+Fill out the function get_last_element(lst) which takes in a list lst as a parameter and prints the last element in the list. The list is guaranteed to be non-empty, but there are no guarantees on its length.
+
+## Starter Code
+
+```bash
+def main():
+    print("Delete this line and write your code here! :)")
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
+```
+
+
+
+
+if __name__ == '__main__':
+    main()
---
+++ Projects/01_Homework_Projects/02_lists/06_get_last_element/main.py
@@ -0,0 +1,26 @@
+""" Problem Statement: Fill out the function get_last_element(lst) which takes in a list lst as a parameter and 
+prints the last element in the list. The list is guaranteed to be
+ non-empty, but there are no guarantees on its length."""
+
+
+def user_prompt():
+    entry = []
+    prompt  = input("Please type your element here ")
+    while prompt != "":
+       entry.append(prompt)
+       prompt  = input("Please type your element here ")
+    return entry
+
+
+def get_last_element(list_of_elements):
+    print(list_of_elements[-1])
+
+def main():
+
+    element = user_prompt()
+    get_last_element(element)
+
+
+
+if __name__ == "__main__":
+    main()
