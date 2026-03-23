9c17433ce3e339b7db625620556a3866aa661a7d
SJcodeML <144626657+SJcodeML@users.noreply.github.com>
2026-03-23 16:49:06 +0500

    code added

--- Projects/01_Homework_Projects/04_dictionaries/03_powerful_passwords/main.py
+++ Projects/01_Homework_Projects/04_dictionaries/03_powerful_passwords/main.py
@@ -1,8 +1,7 @@
-## Problem Statement
+"""want to make a program that can match which password belongs to which website without storing the actual password!
 
-You want to be safe online and use different passwords for different websites. However, you are forgetful at times and want to make a program that can match which password belongs to which website without storing the actual password!
-
-This can be done via something called hashing. Hashing is when we take something and convert it into a different, unique identifier. This is done using a hash function. Luckily, there are several resources that can help us with this.
+This can be done via something called hashing. Hashing is when we take something and convert it into a different,
+ unique identifier. This is done using a hash function. Luckily, there are several resources that can help us with this.
 
 For example, using a hash function called SHA256(...) something as simple as 
 
@@ -12,26 +11,18 @@ can be hashed into a much more complex
 
 2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824
 
-Fill out the login(...) function for a website that hashes their passwords. Login should return True if an email's stored password hash in stored_logins is the same as the hash of password_to_check.
+Fill out the login(...) function for a website that hashes their passwords. Login should return
+True if an email's stored password hash in stored_logins is the same as the hash of password_to_check.
 
-(Hint. You will need to use the provided hash_password(...) function. You don't necessarily need to know how it works, just know that hash_password(...) returns the hash for the password!)
-
-## Starter Code
-
-```bash
-def main():
-    print("Delete this line and write your code here! :)")
+(Hint. You will need to use the provided hash_password(...) function. You don't necessarily need to
+ know how it works, just know that hash_password(...) returns the hash for the password!)
+"""
 
 
-# This provided line is required at the end of
-# Python file to call the main() function.
-if __name__ == '__main__':
-    main()
-```
 
-## Solution
 
-```bash
+
+
 from hashlib import sha256
 
 def login(email, stored_logins, password_to_check):
@@ -75,13 +66,12 @@ def main():
     print(login("example@gmail.com", stored_logins, "word"))
     print(login("example@gmail.com", stored_logins, "password"))
     
-    print(login("code_in_placer@cip.org", stored_logins, "Karel"))
-    print(login("code_in_placer@cip.org", stored_logins, "karel"))
+    # print(login("code_in_placer@cip.org", stored_logins, "Karel"))
+    # print(login("code_in_placer@cip.org", stored_logins, "karel"))
     
-    print(login("student@stanford.edu", stored_logins, "password"))
-    print(login("student@stanford.edu", stored_logins, "123!456?789"))
+    # print(login("student@stanford.edu", stored_logins, "password"))
+    # print(login("student@stanford.edu", stored_logins, "123!456?789"))
 
 
 if __name__ == '__main__':
     main()
-```
