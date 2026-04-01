a830914268747059437fb91c45e016a6b1d6a2e1
SJcodeML <144626657+SJcodeML@users.noreply.github.com>
2026-04-01 18:36:34 +0500

    projects added

---
+++ Projects/01_Homework_Projects/05_loops_control_flow/00_guess_my_number/README.md
@@ -0,0 +1,34 @@
+## Problem Statement
+
+Guess My Number
+
+I am thinking of a number between 0 and 99...
+Enter a guess: 50
+Your guess is too high
+
+Enter a new number: 25
+Your guess is too low
+
+Enter a new number: 40
+Your guess is too low
+
+Enter a new number: 45
+Your guess is too low
+
+Enter a new number: 48
+Congrats! The number was: 48
+
+## Starter Code
+
+```bash
+def main():
+    print("Delete this line and write your code here! :)")
+
+
+# This provided line is required at the end of
+# Python file to call the main() function.
+if __name__ == '__main__':
+    main()
+```
+
+
--- Projects/01_Homework_Projects/05_loops_control_flow/00_guess_my_number/main.py
+++ Projects/01_Homework_Projects/05_loops_control_flow/00_guess_my_number/main.py
@@ -1,38 +1,3 @@
-## Problem Statement
-
-Guess My Number
-
-I am thinking of a number between 0 and 99...
-Enter a guess: 50
-Your guess is too high
-
-Enter a new number: 25
-Your guess is too low
-
-Enter a new number: 40
-Your guess is too low
-
-Enter a new number: 45
-Your guess is too low
-
-Enter a new number: 48
-Congrats! The number was: 48
-
-## Starter Code
-
-```bash
-def main():
-    print("Delete this line and write your code here! :)")
-
-
-# This provided line is required at the end of
-# Python file to call the main() function.
-if __name__ == '__main__':
-    main()
-```
-
-## Solution
-```bash
 import random
 
 def main():
@@ -57,4 +22,3 @@ def main():
     
 if __name__ == '__main__':
     main()
-```
