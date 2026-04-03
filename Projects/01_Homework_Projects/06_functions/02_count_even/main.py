be404970584f984174c6a02fcaf26fd61a30ac48
SJcodeML <144626657+SJcodeML@users.noreply.github.com>
2026-04-03 18:55:43 +0500

    project added

--- Projects/01_Homework_Projects/06_functions/02_count_even/main.py
+++ Projects/01_Homework_Projects/06_functions/02_count_even/main.py
@@ -1,29 +1,3 @@
-## Problem Statement
-
-Fill out the function count_even(lst) which 
-
-first populates a list by prompting the user for integers until they press enter (please use the prompt "Enter an integer or press enter to stop: "), 
-
-and then prints the number of even numbers in the list. 
-
-If you'd prefer to focus on the second task only, scroll down for our implementation of the first task!
-
-
-## Starter Code
-
-```bash
-def main():
-    print("Delete this line and write your code here! :)")
-
-
-# This provided line is required at the end of
-# Python file to call the main() function.
-if __name__ == '__main__':
-    main()
-```
-
-## Solution
-```bash
 
 def count_even(lst):
     """
@@ -67,4 +41,7 @@ def main():
 
 if __name__ == '__main__':
     main()
-```
+
+
+if __name__ == "__main__":
+    main()
